c7d68f21c82ebd705e80942274c0191b1150dda3
MZC0829 <1005756105@qq.com>
2017-03-21 01:19:47 +0800

    整理代码
    整理代码

--- 地图搜索/ViewController.swift
+++ 地图搜索/ViewController.swift
@@ -12,6 +12,28 @@ import MapKit
 class ViewController: UIViewController,UITextFieldDelegate
 {
     @IBOutlet weak var nameButtonSearch: UIButton!
+    @IBOutlet weak var nameTextFieldInputSearchPlace: UITextField!
+    @IBOutlet weak var nameMKMapViewShow: MKMapView!
+    @IBOutlet weak var nameButtonMenu: UIButton!
+    @IBOutlet weak var nameButtonHotel: UIButton!
+    @IBOutlet weak var nameButtonHospital: UIButton!
+    @IBOutlet weak var nameButtonSuperMarket: UIButton!
+    
+    // 设置当前位置
+    let initLocation = CLLocation(latitude: 22.5390170000 , longitude: 113.9369500000)
+    //设置搜索半径
+    let searchRadius:CLLocationDistance = 4000
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        initNameButtonMenu()
+        initNamePlaceType()
+        funcMapDownload()
+        //funcMapDownload(22.5390170000 , longiti: 113.9369500000)
+        initTextField()
+        initButtonSearch()
+    }
     
     //定义温馨全局变量弹出框
     var alertController = UIAlertController()
@@ -22,17 +44,12 @@ class ViewController: UIViewController,UITextFieldDelegate
         {
         case "请输入要搜索的地点":
             alertController = UIAlertController(title: "温馨提示", message: "请输入要搜索的地点", preferredStyle: UIAlertControllerStyle.alert)
-            
         case "还没输入要搜索的地点":
             alertController = UIAlertController(title: "温馨提示", message: "还没输入要搜索的地点", preferredStyle: UIAlertControllerStyle.alert)
-            
         default:
             break
-            
         }
-        
         let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.cancel, handler: nil)
-        
         let okAction = UIAlertAction(title: "好的", style: UIAlertActionStyle.default, handler: {
                     action in
                     print("点击了确定")
@@ -46,20 +63,15 @@ class ViewController: UIViewController,UITextFieldDelegate
     // 搜索按钮 点击事件
     @IBAction func funcButtonSearch(_ sender: AnyObject)
     {
-        
-        
         //收起键盘
         nameTextFieldInputSearchPlace.resignFirstResponder()
-        
         //判断段头段尾空格
         let temp = nameTextFieldInputSearchPlace.text?.trimmingCharacters(in: CharacterSet.whitespaces)
-        
         if (nameTextFieldInputSearchPlace.text?.isEmpty)!
         {
             print("请输入要搜索的地点")
             funcAlertController("请输入要搜索的地点")
             initTextField()
-
         }
         else if (temp == "")
         {
@@ -71,64 +83,36 @@ class ViewController: UIViewController,UITextFieldDelegate
         {
             nameMKMapViewShow.removeAnnotations(nameMKMapViewShow.annotations)
             funcSearchPlaceInMap(nameTextFieldInputSearchPlace.text!)
-
         }
-        
     }
     
-    @IBOutlet weak var nameTextFieldInputSearchPlace: UITextField!
-    
-    @IBOutlet weak var nameMKMapViewShow: MKMapView!
-    
-    // 设置当前位置
-    let initLocation = CLLocation(latitude: 22.5390170000 , longitude: 113.9369500000)
-    //设置搜索半径
-    let searchRadius:CLLocationDistance = 4000
-    
-    @IBOutlet weak var nameButtonMenu: UIButton!
-    
-    //菜单按钮点击事件
+    // MARK: 菜单按钮点击事件
     @IBAction func funcButtonMenu(_ sender: AnyObject)
     {
         UIView.animate(withDuration: 1, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
-            
             self.nameButtonHospital.alpha = 1
             self.nameButtonHotel.alpha = 1
             self.nameButtonSuperMarket.alpha = 1
-            
             self.nameButtonSuperMarket.transform = CGAffineTransform(scaleX: 1.5, y: 1.5).concatenating(CGAffineTransform(translationX: -80, y: -25))
             self.nameButtonHotel.transform = CGAffineTransform(scaleX: 1.5, y: 1.5).concatenating(CGAffineTransform(translationX: 0, y: -80))
             self.nameButtonHospital.transform = CGAffineTransform(scaleX: 1.5, y: 1.5).concatenating(CGAffineTransform(translationX: 80, y: -25))
-            
-           
             self.nameButtonMenu.transform = CGAffineTransform(rotationAngle: 0)
-            
             }, completion: nil)
-
     }
     
-    @IBOutlet weak var nameButtonHotel: UIButton!
-    @IBOutlet weak var nameButtonHospital: UIButton!
-    @IBOutlet weak var nameButtonSuperMarket: UIButton!
-    
-    //点击地点类型按钮事件
-    
+    // MARK: 点击地点类型按钮事件
     //SuperMarket 的tag为1
     //Hospital    的tag为2
     //Hotel       的tag为3
     @IBAction func funcPlaceType(_ sender: AnyObject)
     {
-        
         funcResetButton()
-        
         nameMKMapViewShow.removeAnnotations(nameMKMapViewShow.annotations)
-        
         switch sender.tag
         {
         case 1:
             funcSearchPlaceInMap("Supermarket")
             print("1")
-            
         case 2:
             funcSearchPlaceInMap("hospital")
             print("2")
@@ -136,58 +120,50 @@ class ViewController: UIViewController,UITextFieldDelegate
             funcSearchPlaceInMap("hotel")
             print("3")
         default:
-            return
+            break
         }
     }
     
-    //按照选择的地点类型进行搜索方法
+    // MARK: 按照选择的地点类型进行搜索方法
 //    func dequeueReusableAnnotationViewWithIdentifier(identifier: String!) -> MKAnnotationView
 //    {
 //        return dequeueReusableAnnotationViewWithIdentifier(identifier)
 //    }
     
-    //初始化地点类型按钮
+    // MARK: 初始化地点类型按钮
     func initNamePlaceType()
     {
         self.nameButtonHospital.alpha = 0
         self.nameButtonHotel.alpha = 0
         self.nameButtonSuperMarket.alpha = 0
-        
         //变成圆角
         self.nameButtonHospital.layer.cornerRadius = 10
         self.nameButtonHotel.layer.cornerRadius = 10
         self.nameButtonSuperMarket.layer.cornerRadius = 10
     }
     
-    //初始化菜单按钮
+    // MARK: 初始化菜单按钮
     func initNameButtonMenu()
     {
         self.nameButtonMenu.alpha = 0
         UIView.animate(withDuration: 1, delay: 0.5, options: UIViewAnimationOptions(), animations: { () -> Void in
             self.nameButtonMenu.alpha = 1
             self.nameButtonMenu.transform = CGAffineTransform(rotationAngle: 3.1415927 * 0.25)
-            
             }, completion: nil)
     }
     
-    //点击地点按钮后，还原按钮方法
+    // MARK: 点击地点按钮后，还原按钮方法
     func funcResetButton()
     {
         UIView.animate(withDuration: 1, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
-            
             self.nameButtonHospital.alpha = 0
             self.nameButtonHotel.alpha = 0
             self.nameButtonSuperMarket.alpha = 0
-            
             self.nameButtonSuperMarket.transform = CGAffineTransform(scaleX: 1, y: 1).concatenating(CGAffineTransform(translationX: 0, y: 0))
             self.nameButtonHotel.transform = CGAffineTransform(scaleX: 1, y: 1).concatenating(CGAffineTransform(translationX: 0, y: 0))
             self.nameButtonHospital.transform = CGAffineTransform(scaleX: 1, y: 1).concatenating(CGAffineTransform(translationX: 0, y: 0))
-            
-            
             self.nameButtonMenu.transform = CGAffineTransform(rotationAngle: 3.1415927 * 0.25)
-            
             }, completion: nil)
-
     }
     
 //    //地图加载
@@ -202,14 +178,14 @@ class ViewController: UIViewController,UITextFieldDelegate
 //        
 //    }
     
-    //地图加载
+    // MARK: 地图加载
     func funcMapDownload()
     {
         let region = MKCoordinateRegionMakeWithDistance(initLocation.coordinate, searchRadius, searchRadius)
         nameMKMapViewShow.setRegion(region, animated: true)
     }
     
-    //增加兴趣地点
+    // MARK: 增加兴趣地点
     func funcAddLocation(_ title:String,latitude:CLLocationDegrees,longitude:CLLocationDegrees)
     {
         let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
@@ -217,7 +193,7 @@ class ViewController: UIViewController,UITextFieldDelegate
         nameMKMapViewShow.addAnnotation(myAnnotation)
     }
     
-    //搜索方法
+    // MARK: 搜索方法
     func funcSearchPlaceInMap(_ place:String)
     {
         let request = MKLocalSearchRequest()
@@ -232,10 +208,7 @@ class ViewController: UIViewController,UITextFieldDelegate
             {
                 self.funcAddLocation(item.name!, latitude: (item.placemark.location?.coordinate.latitude)!, longitude: (item.placemark.location?.coordinate.longitude)!)
             }
-            
         }
-        
-
         
 //        search.startWithCompletionHandler
 //            {(response:MKLocalSearchResponse!,error: NSError!) -> Void in
@@ -246,13 +219,12 @@ class ViewController: UIViewController,UITextFieldDelegate
 //        }
     }
     
-    //初始化textfield
+    // MARK: 初始化textfield
     func initTextField()
     {
         nameTextFieldInputSearchPlace.placeholder = "输入要搜索的地点"
         //页面加载完成就获得焦点
         nameTextFieldInputSearchPlace.becomeFirstResponder()
-        
         //nameTextFieldInputSearchPlace.returnKeyType = UIReturnKeyType.Search
         nameTextFieldInputSearchPlace.delegate = self
     }
@@ -265,33 +237,16 @@ class ViewController: UIViewController,UITextFieldDelegate
         return true
     }
     
-    //初始化ButtonSearch
+    // MARK: 初始化ButtonSearch
     func initButtonSearch()
     {
         self.nameButtonSearch.layer.borderWidth = 1
         self.nameButtonSearch.layer.cornerRadius = 5
-        
     }
     
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        
-        initNameButtonMenu()
-        initNamePlaceType()
-        funcMapDownload()
-        //funcMapDownload(22.5390170000 , longiti: 113.9369500000)
-        
-        initTextField()
-        initButtonSearch()
-    }
-
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        
     }
-
-
 }
 
